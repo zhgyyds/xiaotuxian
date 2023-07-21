61c2355bbb52205b257bd376a14a0b4552fd8688
zhgyyds <3191714158@qq.com>
2023-07-21 11:14:29 +0800

    调整目录结构

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,80 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+// export default createStore({
+//     state: {
+//         username: 'zs'
+//     },
+//     getters: {
+//         newName(state) {
+//             return state.username + '!!!'
+
+//         }
+//     },
+//     mutations: {
+//         updateName(state) {
+//             state.username = 'Is'
+//         }
+//     },
+//     actions: {
+//         updateName(ctx) {
+//             setTimeout(() => {
+//                 ctx.commit('updateName')
+//             }, 1000)
+//         }
+//     },
+//     modules: {}
+// })
+
+const moduleA = {
   state: {
+    username: 'moduleA'
   },
   getters: {
+    newName (state) {
+      return state.username + '!!!'
+    }
   },
   mutations: {
+    updateName (state) {
+      state.updateName = 'moduleAAA'
+    }
   },
   actions: {
+    updateName (ctx) {
+      setTimeout(() => {
+        ctx.commit('updateName')
+      }, 1000)
+    }
+  }
+
+}
+
+const moduleB = {
+  namespaced: true,
+  state: {
+    username: 'moduleB'
   },
+  getters: {
+    newName (state) {
+      return state.username + '!!!'
+    }
+  },
+  mutations: {
+    updateName (state) {
+      state.username = 'Is'
+    }
+  },
+  actions: {
+    updateName (ctx) {
+      setTimeout(() => {
+        ctx.commit('updateName')
+      }, 1000)
+    }
+  }
+}
+export default createStore({
   modules: {
+    moduleA,
+    moduleB
   }
 })
---
+++ src/根模块的vuex的用法.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    app
+    <p>{{ $store.state.username }}</p>
+    <p>{{ $store.getters.newName }}</p>
+    <p>{{ $store.getters["newName"] }}</p>
+    <button @click="change">修改</button>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup () {
+    const store = useStore()
+    console.log(store.state.username)
+    console.log(store.getters.newName)
+
+    const change = () => {
+      // store.commit("updateName");
+      store.dispatch('updateName')
+    }
+    return { change }
+  }
+}
+</script>
+
+<style></style>
