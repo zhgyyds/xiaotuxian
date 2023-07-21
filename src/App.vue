c748c4422ac6400c79afe1395dcb7e5aa514cbfa
zhgyyds <3191714158@qq.com>
2023-07-21 16:02:39 +0800

    请求函数完成

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <template>
   <div>
-    <p>{{ $store.state.moduleA.username }}</p>
-    <p>{{ $store.getters.newName }}</p>
-
-    <p>{{ $store.state.moduleB.username }}</p>
-    <p>{{ $store.getters["moduleB/newName"] }}</p>
-    <button @click="mutationFn">mutattionsFn</button>
-    <button @click="actionFn">actionFn</button>
+    {{ $store.state.user.profile.account }}
+    <button @click="$store.commit('user/setUser', { account: 'zhg' })">
+      设置用户信息
+    </button>
   </div>
 </template>
 
 <script>
-import { useStore } from 'vuex'
 export default {
   name: 'App',
-  setup () {
-    const store = useStore()
-    const mutationFn = () => {
-      store.commit('moduleB/updateName')
-    }
-    const actionFn = () => {
-      store.dispatch('moduleB/updateName')
-    }
-    return { mutationFn, actionFn }
-  }
+  setup () {}
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -25,56 +25,68 @@ import { createStore } from 'vuex'
 //     modules: {}
 // })
 
-const moduleA = {
-  state: {
-    username: 'moduleA'
-  },
-  getters: {
-    newName (state) {
-      return state.username + '!!!'
-    }
-  },
-  mutations: {
-    updateName (state) {
-      state.updateName = 'moduleAAA'
-    }
-  },
-  actions: {
-    updateName (ctx) {
-      setTimeout(() => {
-        ctx.commit('updateName')
-      }, 1000)
-    }
-  }
+// const moduleA = {
+//   state: {
+//     username: 'moduleA'
+//   },
+//   getters: {
+//     newName (state) {
+//       return state.username + '!!!'
+//     }
+//   },
+//   mutations: {
+//     updateName (state) {
+//       state.updateName = 'moduleAAA'
+//     }
+//   },
+//   actions: {
+//     updateName (ctx) {
+//       setTimeout(() => {
+//         ctx.commit('updateName')
+//       }, 1000)
+//     }
+//   }
 
-}
+// }
 
-const moduleB = {
-  namespaced: true,
-  state: {
-    username: 'moduleB'
-  },
-  getters: {
-    newName (state) {
-      return state.username + '!!!'
-    }
-  },
-  mutations: {
-    updateName (state) {
-      state.username = 'Is'
-    }
-  },
-  actions: {
-    updateName (ctx) {
-      setTimeout(() => {
-        ctx.commit('updateName')
-      }, 1000)
-    }
-  }
-}
+// const moduleB = {
+//   namespaced: true,
+//   state: {
+//     username: 'moduleB'
+//   },
+//   getters: {
+//     newName (state) {
+//       return state.username + '!!!'
+//     }
+//   },
+//   mutations: {
+//     updateName (state) {
+//       state.username = 'Is'
+//     }
+//   },
+//   actions: {
+//     updateName (ctx) {
+//       setTimeout(() => {
+//         ctx.commit('updateName')
+//       }, 1000)
+//     }
+//   }
+// }
+
+import cart from './modules/cart'
+import user from './modules/user'
+import category from './modules/category'
+import createPersistedstate from 'vuex-persistedstate'
 export default createStore({
   modules: {
-    moduleA,
-    moduleB
-  }
+    user,
+    cart,
+    category
+  },
+  plugins: [
+    createPersistedstate({
+      key: 'erabbit-client-pc-store',
+      paths: ['user', 'cart']
+    })
+  ]
 })
